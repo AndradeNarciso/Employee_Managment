3b30a59a61e468437a22d3a2e3b5d03032ab3f59
AndradeNarciso <andradegregorio277@gmail.com>
2026-04-13 22:51:42 +0200

    refactor: Nav and footer added

--- employee_front/src/app/app.ts
+++ employee_front/src/app/app.ts
@@ -2,11 +2,13 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EmployeeList } from './components/employee-list/employee-list';
 import { HttpClientModule } from '@angular/common/http';
+import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EmployeeList, HttpClientModule],
+  imports: [RouterOutlet, EmployeeList, HttpClientModule, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

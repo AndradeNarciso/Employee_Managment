baf8a7450290740bfafc6b01d2d279ec7aa82ab2
AndradeNarciso <andradegregorio277@gmail.com>
2026-04-13 23:20:20 +0200

    refactor: Removed components nav and footer

--- employee_front/src/app/app.ts
+++ employee_front/src/app/app.ts
@@ -2,16 +2,16 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EmployeeList } from './components/employee-list/employee-list';
 import { HttpClientModule } from '@angular/common/http';
-import { Navbar } from './components/navbar/navbar';
-import { Footer } from './components/footer/footer';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EmployeeList, HttpClientModule, Navbar, Footer],
+  imports: [RouterOutlet, EmployeeList, HttpClientModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  currentYear = new Date().getFullYear();
   protected readonly title = signal('employee_front');
 }

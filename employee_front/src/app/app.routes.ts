80375916938532a20fbdafd2488c61ae89ac47d1
AndradeNarciso <andradegregorio277@gmail.com>
2026-04-13 22:10:33 +0200

    feat: redirect added, from now on / redirect to Employee list  by default

--- employee_front/src/app/app.routes.ts
+++ employee_front/src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { EmployeeList } from './components/employee-list/employee-list';
 
 export const routes: Routes = [{
-    path: 'employee-list', component: EmployeeList
-}];
+    path: 'employees', component: EmployeeList
+},
+{ path: '', redirectTo: 'employees', pathMatch: 'full' }];
